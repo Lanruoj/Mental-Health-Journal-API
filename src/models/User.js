61e2ea7ca69f6e8b308493dfaaeea353de49a2c2
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-01-18 14:03:21 +1100

    Separate fullName property to firstName & lastName for User model

--- src/models/User.js
+++ src/models/User.js
@@ -4,7 +4,8 @@ const UserSchema = new mongoose.Schema({
   email: { type: String, required: true },
   password: { type: String, minLength: 8 },
   username: { type: String, minLength: 3 },
-  fullName: String,
+  firstName: String,
+  lastName: String,
   role: { type: mongoose.Types.ObjectId, ref: "Role" },
   posts: [{ type: mongoose.Types.ObjectId, ref: "Post" }],
 });
