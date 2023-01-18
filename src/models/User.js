db082c116081522dc9853308e340ec832f81f97d
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-01-18 12:26:03 +1100

    Create User schema & model

--- src/models/User.js
+++ src/models/User.js
@@ -2,4 +2,13 @@ const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
   email: { type: String, required: true },
+  password: { type: String, minLength: 8 },
+  username: { type: String, minLength: 3 },
+  fullName: String,
+  role: { type: mongoose.Types.ObjectId, ref: "Role" },
+  posts: [{ type: mongoose.Types.ObjectId, ref: "Post" }],
 });
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = { User };
