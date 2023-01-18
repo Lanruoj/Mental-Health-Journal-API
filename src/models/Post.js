1a1f7a8705ea475ded97baecb46ab818b40489be
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-01-18 12:39:02 +1100

    Create Post model & schema

---
+++ src/models/Post.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const PostSchema = new mongoose.Schema({
+  date: Date,
+  moodRating: { type: Number, min: 0, max: 10 },
+  description: { type: String, maxLength: 500 },
+  actionPlan: { type: String, maxLength: 500 },
+  author: { type: mongoose.Types.ObjectId, ref: "User" },
+});
+
+const Post = mongoose.model("Post", PostSchema);
+
+module.exports = { Post };
